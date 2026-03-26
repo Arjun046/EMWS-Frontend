63136013d21d249955cdf20564d08510ea1c7a0b
Arjun Suthar <arjunsuthar2001@gmail.com>
2026-03-26 21:46:20 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   appName: 'EWMS Workforce Console',
-  apiBaseUrl: 'https://seven-chicken-tie.loca.lt',
-  widgetSocketUrl: 'wss://seven-chicken-tie.loca.lt/ws-hub',
-  chatSocketUrl: 'wss://seven-chicken-tie.loca.lt/ws-chat'
+  apiBaseUrl: 'https://download-cameras-travis-castle.trycloudflare.com',
+  widgetSocketUrl: 'wss://download-cameras-travis-castle.trycloudflare.com/ws-hub',
+  chatSocketUrl: 'wss://download-cameras-travis-castle.trycloudflare.com/ws-chat'
 };
